4adcb260c7758f02b5b6073d834031f2e961b167
olivier van den broek <olivier.vdbroek@gmail.com>
2018-11-23 15:08:25 +0100

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   resources :cocktails, only: [:new, :index, :create, :show] do
     resources :doses, only: [:new, :create, :destroy]
   end
